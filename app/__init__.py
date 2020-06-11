d42bfbcd0ab255a3f9fcc2233852211cfc6bd885
marcelomamc <marcelomamc@fab.mil.br>
2020-06-11 09:44:32 -0300

    Atualização

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify, render_template
 from flaskext.mysql import MySQL
 from datetime import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/address/api/latlong": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
